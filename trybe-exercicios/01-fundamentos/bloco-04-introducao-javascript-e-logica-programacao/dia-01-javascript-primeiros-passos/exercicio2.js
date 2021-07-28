9b65549f3372ec181655e8c6e3b91e36375b4bb2
Joao Marco <joaomarco.matta@gmail.com>
2021-07-28 18:40:53 -0300

    Implementa exercicio testa positivo, negativo ou zero.

--- trybe-exercicios/01-fundamentos/bloco-04-introducao-javascript-e-logica-programacao/dia-01-javascript-primeiros-passos/exercicio2.js
+++ trybe-exercicios/01-fundamentos/bloco-04-introducao-javascript-e-logica-programacao/dia-01-javascript-primeiros-passos/exercicio2.js
@@ -5,6 +5,7 @@ let b = 10;
 
 if (a > b) {
     console.log(a);
-} else {
+} 
+else {
     console.log(b);
 }
---
+++ trybe-exercicios/01-fundamentos/bloco-04-introducao-javascript-e-logica-programacao/dia-01-javascript-primeiros-passos/exercicio4.js
@@ -0,0 +1,13 @@
+// Checa se o número é positivo, negativo ou igual a zero.
+
+let num = 0;
+
+if (num > 0) {
+    console.log("positive");
+}
+else if (num < 0) {
+    console.log("negativo");
+}
+else {
+    console.log("zero");
+}
