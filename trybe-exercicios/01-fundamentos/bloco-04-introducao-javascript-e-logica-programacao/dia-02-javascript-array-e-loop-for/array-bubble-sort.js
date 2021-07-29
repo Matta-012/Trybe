cf2542143c44ee1c8561cf46806cf92703340a38
Joao Marco <joaomarco.matta@gmail.com>
2021-07-29 20:07:02 -0300

    Cria um array com a multiplicaçao de outro array.

--- trybe-exercicios/01-fundamentos/bloco-04-introducao-javascript-e-logica-programacao/dia-02-javascript-array-e-loop-for/array-bubble-sort.js
+++ trybe-exercicios/01-fundamentos/bloco-04-introducao-javascript-e-logica-programacao/dia-02-javascript-array-e-loop-for/array-bubble-sort.js
@@ -15,17 +15,33 @@
 // }
 
 //2-Ordena o array em ordem DECRESCENTE com o BUBBLE SORT e imprime na tela.
-let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let index = 1; index < array.length; index += 1) {
-    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-        if (array[index] > array[secondIndex]) {
-            let position = array[index];
-            array[index] = array[secondIndex];
-            array[secondIndex] = position;
+// for (let index = 1; index < array.length; index += 1) {
+//     for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+//         if (array[index] > array[secondIndex]) {
+//             let position = array[index];
+//             array[index] = array[secondIndex];
+//             array[secondIndex] = position;
+//         }
+//     }
+// }
+// for (let number of array) {
+//     console.log(number);
+// }
+
+//3-Cria um segundo array, a partir do anterior, com o valor da posição atual vezes o próximo.
+let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let arrOfMultiplication = [];
+
+for (let index = 0; index < array.length; index += 1) {
+        if (index < array.length - 1) {
+            arrOfMultiplication.push(array[index] * array[index + 1]);
         }
-    }
+        else {
+            arrOfMultiplication.push(array[index] * 2);
+        }
 }
-for (let number of array) {
+for (let number of arrOfMultiplication) {
     console.log(number);
 }
