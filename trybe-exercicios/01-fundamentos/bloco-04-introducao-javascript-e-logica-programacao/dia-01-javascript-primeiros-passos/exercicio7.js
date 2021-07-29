516449ae86d3d6e0e125dd508fb61b705b959e8d
Joao Marco <joaomarco.matta@gmail.com>
2021-07-29 11:07:49 -0300

    Implementa exercicio conversao de notas.

---
+++ trybe-exercicios/01-fundamentos/bloco-04-introducao-javascript-e-logica-programacao/dia-01-javascript-primeiros-passos/exercicio7.js
@@ -0,0 +1,27 @@
+let nota = 85;
+
+if (nota >= 0 && nota <=100) {
+    if (nota >= 90) {
+        nota = "A";
+    }
+    else if (nota >= 80 && nota < 90) {
+        nota = "B";
+    }
+    else if (nota >= 70 && nota < 80) {
+        nota = "C";
+    }
+    else if (nota >= 60 && nota < 70) {
+        nota = "D";
+    }
+    else if (nota >= 50 && nota < 60) {
+        nota = "E";
+    }
+    else {
+        nota = "F";
+    }
+    console.log(nota);
+}
+else {
+    console.log("nota inválida.");
+}
+
