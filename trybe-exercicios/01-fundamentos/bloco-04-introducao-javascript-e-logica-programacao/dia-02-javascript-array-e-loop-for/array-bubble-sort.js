0f7a52c36d87b340fb2712d7d7ec175278fbd873
Joao Marco <joaomarco.matta@gmail.com>
2021-07-29 18:52:20 -0300

    Ordena array de forma crescente e decrescente.

---
+++ trybe-exercicios/01-fundamentos/bloco-04-introducao-javascript-e-logica-programacao/dia-02-javascript-array-e-loop-for/array-bubble-sort.js
@@ -0,0 +1,31 @@
+//1-Ordena o array em ordem CRESCENTE com o BUBBLE SORT e imprime na tela.
+// let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let index = 1; index < array.length; index += 1) {
+//     for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+//         if (array[index] < array[secondIndex]) {
+//             let position = array[index];
+//             array[index] = array[secondIndex];
+//             array[secondIndex] = position;
+//         }
+//     }
+// }
+// for (let number of array) {
+//     console.log(number);
+// }
+
+//2-Ordena o array em ordem DECRESCENTE com o BUBBLE SORT e imprime na tela.
+let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 1; index < array.length; index += 1) {
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+        if (array[index] > array[secondIndex]) {
+            let position = array[index];
+            array[index] = array[secondIndex];
+            array[secondIndex] = position;
+        }
+    }
+}
+for (let number of array) {
+    console.log(number);
+}
