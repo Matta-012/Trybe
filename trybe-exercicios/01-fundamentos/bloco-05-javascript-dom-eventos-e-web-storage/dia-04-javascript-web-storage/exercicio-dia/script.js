5bcfb99435cb7ee1cb95202801a60ab0dae1c8ce
Joao Marco <joaomarco.matta@gmail.com>
2021-08-13 09:11:32 -0300

    Implementa mudança de cor do background.

--- trybe-exercicios/01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/exercicio-dia/script.js
+++ trybe-exercicios/01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/exercicio-dia/script.js
@@ -1,6 +1,26 @@
 const getBackgroundColor = document.getElementById('container');
 const getParagraph = document.getElementById('journalParagraph');
+const getBackgroundInput = document.getElementById('background');
+const getColorInput = document.getElementById('textColor');
+const getFontSizeInput = document.getElementById('fontSize');
+const getLineHeightInput = document.getElementById('lineHeight');
+const getFontFamilyInput = document.getElementById('fontFamily');
 
 window.onload = function() {
-    
+    restoreBackgroundStorage;
+    console.log(restoreBackgroundStorage);
+}
+
+function handleBackgroundColorChange() {
+    getBackgroundColor.style.backgroundColor = getBackgroundInput.value;
+
+    const color = getBackgroundInput.value;
+    localStorage.setItem('colors', JSON.stringify(color));
+}
+getBackgroundInput.addEventListener('keyup', handleBackgroundColorChange);
+
+function restoreBackgroundStorage() {
+    const color = JSON.parse(localStorage.getItem('colors'));
+
+    getBackgroundColor.style.backgroundColor = color;
 }
