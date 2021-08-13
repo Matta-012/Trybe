9207ec6c1458ea293b589f32f6c4a349f5393f70
Joao Marco <joaomarco.matta@gmail.com>
2021-08-13 11:27:31 -0300

    Implementa mudança de cor no bg e no text e salva no localStorage.

--- trybe-exercicios/01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/exercicio-dia/script.js
+++ trybe-exercicios/01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/exercicio-dia/script.js
@@ -5,22 +5,42 @@ const getColorInput = document.getElementById('textColor');
 const getFontSizeInput = document.getElementById('fontSize');
 const getLineHeightInput = document.getElementById('lineHeight');
 const getFontFamilyInput = document.getElementById('fontFamily');
+const bgButton = document.getElementById('bgButton');
+const textButton = document.getElementById('textButton');
+const sizeButton = document.getElementById('sizeButton');
+const lineButton = document.getElementById('lineButton');
+const fontButton = document.getElementById('fontButton');
+let styleChanges = {};
 
-window.onload = function() {
-    restoreBackgroundStorage;
-    console.log(restoreBackgroundStorage);
-}
-
+//Implementa mudança de cor do background,salva no localStorage e depois recupera ela.
 function handleBackgroundColorChange() {
-    getBackgroundColor.style.backgroundColor = getBackgroundInput.value;
+    styleChanges.background = getBackgroundInput.value;
+    getBackgroundColor.style.backgroundColor = styleChanges.background;
 
-    const color = getBackgroundInput.value;
-    localStorage.setItem('colors', JSON.stringify(color));
+    localStorage.setItem('background', JSON.stringify(styleChanges.background));
 }
-getBackgroundInput.addEventListener('keyup', handleBackgroundColorChange);
+bgButton.addEventListener('click', handleBackgroundColorChange);
 
 function restoreBackgroundStorage() {
-    const color = JSON.parse(localStorage.getItem('colors'));
-
+    const color = JSON.parse(localStorage.getItem('background'));
     getBackgroundColor.style.backgroundColor = color;
+}
+
+//Implementa mudança de cor no text, salva no localStorage e depois recupera ela.
+function handleTextColorChange() {
+    styleChanges.text = getColorInput.value;
+    getParagraph.style.color = styleChanges.text;
+
+    localStorage.setItem('text', JSON.stringify(styleChanges.text));
+}
+textButton.addEventListener('click', handleTextColorChange);
+
+function restoreTextColorStorage() {
+    const color = JSON.parse(localStorage.getItem('text'));
+    getParagraph.style.color = color;
+}
+
+window.onload = function() {
+    restoreBackgroundStorage();
+    restoreTextColorStorage();
 }
