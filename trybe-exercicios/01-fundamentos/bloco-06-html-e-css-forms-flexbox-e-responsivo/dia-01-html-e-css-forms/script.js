66741a62370676af11a73e793ac36c1cf581f8de
Joao Marco <joaomarco.matta@gmail.com>
2021-08-19 15:53:47 -0300

    Cria funçao que preenche todos os estados no form.

--- trybe-exercicios/01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/script.js
+++ trybe-exercicios/01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/script.js
@@ -1,21 +1,35 @@
 function createBrazilStates() {
-    const states = [];
+    const states = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal'];
+    const initials = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO', 'DF'];
+    const dropDownStates = document.getElementById('estado');
+
+    for (let i in states) {
+        const newState = document.createElement('option');
+        newState.innerText = states[i];
+        newState.value = initials[i];
+        dropDownStates.appendChild(newState);
+    }
 }
 
+function verifyDate() {
+    let isValidDate = true;
+    const date = document.getElementById('data-inicio').value;
 
-
+    console.log(date);
+    if (!isValidDate) { return alert('Data inválida!'); }
+}
 
 function handleSubmitBtn(event) {
     event.preventDefault();
-
-    const nome = document.getElementById('nome').value;
 }
 
 function addSubmitBtnEvent() {
     const submitBtn = document.getElementById('enviar');
     submitBtn.addEventListener('click', handleSubmitBtn);
+    submitBtn.addEventListener('click', verifyDate);
 }
 
 window.onload = function() {
     addSubmitBtnEvent();
+    createBrazilStates();
 }
