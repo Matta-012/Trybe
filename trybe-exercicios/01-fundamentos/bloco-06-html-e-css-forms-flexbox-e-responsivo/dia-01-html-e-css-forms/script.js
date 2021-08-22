f21c8b6ac8615f0530e8a19a0f9920d0a68507e5
Joao Marco <joaomarco.matta@gmail.com>
2021-08-22 13:15:19 -0300

    Implementa validaçao do campo data.

--- trybe-exercicios/01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/script.js
+++ trybe-exercicios/01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/script.js
@@ -11,12 +11,45 @@ function createBrazilStates() {
     }
 }
 
-function verifyDate() {
+// Verifica se o dia é válido
+function isValidDay(day) {
+    const intDay = parseInt(day, 10);
+
+    return intDay > 0 && intDay <= 31;
+}
+
+// Verifica se o mês é válido
+function isValidMonth(month) {
+    const intMonth = parseInt(month, 10);
+
+    return intMonth > 0 && intMonth <= 12;
+}
+
+// Verifica se o ano é válido
+function isValidYear(year) {
+    const intyear = parseInt(year, 10);
+
+    return intyear > 0;
+}
+
+function isValidDate() {
     let isValidDate = true;
     const date = document.getElementById('data-inicio').value;
 
-    console.log(date);
+    const dateInput = date.split('/');
+    const day = dateInput[0];
+    const month = dateInput[1];
+    const year = dateInput[2];
+
+    isValidDate = isValidDay(day) && isValidMonth(month) && isValidYear(year);
+    
     if (!isValidDate) { return alert('Data inválida!'); }
+
+    return date;
+}
+
+function isValidField(input, maxLength) {
+    return input.length > 0 && input.length < maxLength;
 }
 
 function handleSubmitBtn(event) {
@@ -26,7 +59,7 @@ function handleSubmitBtn(event) {
 function addSubmitBtnEvent() {
     const submitBtn = document.getElementById('enviar');
     submitBtn.addEventListener('click', handleSubmitBtn);
-    submitBtn.addEventListener('click', verifyDate);
+    submitBtn.addEventListener('click', isValidDate);
 }
 
 window.onload = function() {
