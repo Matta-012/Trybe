aacd84132c5772581343e11fdcec178098c9755c
Joao Marco <joaomarco.matta@gmail.com>
2021-09-10 20:05:42 -0300

    Implementa exercicio 8.

---
+++ trybe-exercicios/01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios-dia/exercicio-8.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+// 8 - Escreva uma função greet que, dado o nome de uma pessoa, retorna uma mensagem de cumprimento:
+
+// escreva greet abaixo
+const greet = (name, greeting = 'Hi') => {
+  return `${greeting} ${name}`;
+}
+
+assert.strictEqual(greet('John'), 'Hi John');
+assert.strictEqual(greet('John', 'Good morning'), 'Good morning John');
+assert.strictEqual(greet('Isabela', 'Oi'), 'Oi Isabela');
