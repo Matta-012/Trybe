d1ed20c6e0de7d4b6cef64d510c86e4d3a804ec2
Joao Marco <joaomarco.matta@gmail.com>
2021-09-10 13:34:00 -0300

    Remove exemplos sobre destructuring deste arquivo.

--- trybe-exercicios/01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/conteudo-dia/spread-operator.js
+++ trybe-exercicios/01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/conteudo-dia/spread-operator.js
@@ -1,8 +1,8 @@
 // Faça uma lista com as suas frutas favoritas
-const specialFruit = ['kiwi', 'pêssego', 'lichia'];
+const specialFruit = ["kiwi", "pêssego", "lichia"];
 
 // Faça uma lista de complementos que você gostaria de adicionar
-const additionalItens = ['bacon', 'pizza', 'lanche'];
+const additionalItens = ["bacon", "pizza", "lanche"];
 
 const fruitSalad = (fruit, additional) => {
   // Esreva sua função aqui
