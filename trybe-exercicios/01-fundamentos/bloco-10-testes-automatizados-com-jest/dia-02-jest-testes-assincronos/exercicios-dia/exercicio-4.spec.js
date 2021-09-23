52f4d208af33b14de40835f37bd7c642f024848d
Joao Marco <joaomarco.matta@gmail.com>
2021-09-22 22:48:01 -0300

    Implementa exercicio 5.

--- trybe-exercicios/01-fundamentos/bloco-10-testes-automatizados-com-jest/dia-02-jest-testes-assincronos/exercicios-dia/exercicio-4.spec.js
+++ trybe-exercicios/01-fundamentos/bloco-10-testes-automatizados-com-jest/dia-02-jest-testes-assincronos/exercicios-dia/exercicio-4.spec.js
@@ -11,9 +11,9 @@ const getRepos = (url) => {
 describe('Testa se getRepos() busca o repositório correto', () => {
   const url = "https://api.github.com/orgs/tryber/repos";
   it('Testa se getRepos("https://api.github.com/orgs/tryber/repos") contém o repositório "sd-01-week4-5-project-todo-list" e "sd-01-week4-5-project-meme-generator"', async () => {
-      const data = await getRepos(url);
-      expect(data).toContainEqual('sd-01-week4-5-project-todo-list');
-      expect(data).toContainEqual('sd-01-week4-5-project-meme-generator');
+      const data = getRepos(url);
+      expect(data).resolves.toContainEqual('sd-01-week4-5-project-todo-list');
+      expect(data).resolves.toContainEqual('sd-01-week4-5-project-meme-generator');
   });
   // Implementa resolução do gabarito
   it('gets a list of repositories names', () => {
