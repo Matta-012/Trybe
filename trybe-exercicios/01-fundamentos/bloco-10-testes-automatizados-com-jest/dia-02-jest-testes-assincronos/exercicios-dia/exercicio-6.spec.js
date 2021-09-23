5e6845e39a5c7c6c22f7db2967c31b70a26d6e67
Joao Marco <joaomarco.matta@gmail.com>
2021-09-23 10:24:42 -0300

    Implementa exercicio 6.2.

--- trybe-exercicios/01-fundamentos/bloco-10-testes-automatizados-com-jest/dia-02-jest-testes-assincronos/exercicios-dia/exercicio-6.spec.js
+++ trybe-exercicios/01-fundamentos/bloco-10-testes-automatizados-com-jest/dia-02-jest-testes-assincronos/exercicios-dia/exercicio-6.spec.js
@@ -1,7 +1,7 @@
 const Animals = [
   { name: 'Dorminhoco', age: 1, type: 'Dog' },
   { name: 'Soneca', age: 2, type: 'Dog' },
-  { name: 'Preguiça', age: 5, type: 'Cat' },
+  { name: 'Preguiça', age: 1, type: 'Cat' },
 ];
 
 // const findAnimalsByType = (type) => (
@@ -38,21 +38,50 @@ const findAnimalByName = (name) => (
 
 // ---------------------
 
-describe('Testando promise - findAnimalByName', () => {
-  describe('Quando existe o animal com o nome procurado', () => {
-    test('Retorne o objeto do animal', () => (
-      findAnimalByName('Dorminhoco').then(animal => {
-        expect(animal).toEqual({ name: 'Dorminhoco', age: 1, type: 'Dog' });
-      })
-    ));
-  });
+// describe('Testando promise - findAnimalByName', () => {
+//   describe('Quando existe o animal com o nome procurado', () => {
+//     test('Retorne o objeto do animal', () => (
+//       findAnimalByName('Dorminhoco').then(animal => {
+//         expect(animal).toEqual({ name: 'Dorminhoco', age: 1, type: 'Dog' });
+//       })
+//     ));
+//   });
 
-  describe('Quando não existe o animal com o nome procurado', () => {
-    test('Retorna um erro', () => {
-      expect.assertions(1);
-      return findAnimalByName('Bob').catch((error) => {
-        expect(error.message).toEqual('Nenhum animal com esse nome!');
-      });
-    });
-  });
+//   describe('Quando não existe o animal com o nome procurado', () => {
+//     test('Retorna um erro', () => {
+//       expect.assertions(1);
+//       return findAnimalByName('Bob').catch((error) => {
+//         expect(error.message).toEqual('Nenhum animal com esse nome!');
+//       });
+//     });
+//   });
+// });
+
+/* ***************************************************************************************************** */
+
+const findAnimalByAge = (age) => (
+  // Adicione o código aqui.
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const animal = Animals.filter((animal) => animal.age === age);
+
+      if (animal.length > 0) return resolve(animal);
+
+      return reject(new Error('Nenhum animal com essa idade!'));
+    }, 100);
+  })
+);
+
+describe('Testando Promise findAnimalByAge()', () => {
+  it('Retorna um array de objetos quando uma idade válida é passada', async () => {
+    const data = await findAnimalByAge(1);
+    expect(data).toHaveLength(2);
+    expect(data[0]).toEqual({ name: 'Dorminhoco', age: 1, type: 'Dog' });
+    expect(data[1]).toEqual({ name: 'Preguiça', age: 1, type: 'Cat' });
+  })
+
+  it('Retorna um erro quando um parâmetro inválido é passado', async () => {
+    expect.assertions(1);
+    return expect(findAnimalByAge(10)).rejects.toThrowError('Nenhum animal com essa idade!');
+  })
 });
