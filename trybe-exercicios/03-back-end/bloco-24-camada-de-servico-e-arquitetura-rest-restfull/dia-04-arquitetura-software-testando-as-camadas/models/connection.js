3b259be5ae31cd1cc4320eb6671bf0915e9cf523
Joao Marco <joaomarco.matta@gmail.com>
2022-03-08 10:08:36 -0300

    adiciona novos arquivos

--- trybe-exercicios/03-back-end/bloco-24-camada-de-servico-e-arquitetura-rest-restfull/dia-04-arquitetura-software-testando-as-camadas/models/connection.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2/promise');
-
-const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'matta',
-  password: 'M@tta012',
-  database: 'model_example'});
-
-module.exports = connection;
